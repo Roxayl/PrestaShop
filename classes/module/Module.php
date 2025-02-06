2b966bdee819fdc23c5241b63e2c59c6ae20aa2b
Codencode <info@codencode.it>
2025-02-06 09:09:47 +0100

    Avoid duplicates in possible module hooks
    Filter out already existing hooks before adding those retrieved from getWidgetHooks to prevent duplicates.

--- classes/module/Module.php
+++ classes/module/Module.php
@@ -3319,16 +3319,18 @@ abstract class ModuleCore implements ModuleInterface
         } while ($splDir->getRealPath() !== $directoryOverride);
     }
 
-    private function getWidgetHooks()
+    private function getWidgetHooks($existing_hook_ids = [])
     {
         $hooks = array_values(Hook::getHooks(false, true));
         $registeredHookList = Hook::getHookModuleList();
 
-        foreach ($hooks as &$hook) {
-            $hook['registered'] = !empty($registeredHookList[$hook['id_hook']][$this->id]);
-        }
-
-        return $hooks;
+        return array_filter(array_map(function ($hook) use ($registeredHookList, $existing_hook_ids) {
+            if (!in_array($hook['id_hook'], $existing_hook_ids)) {
+                $hook['registered'] = !empty($registeredHookList[$hook['id_hook']][$this->id]);
+                return $hook;
+            }
+            return null;
+        }, $hooks));
     }
 
     /**
@@ -3356,7 +3358,7 @@ abstract class ModuleCore implements ModuleInterface
         }
 
         if ($this instanceof WidgetInterface) {
-            $possible_hooks_list = array_merge($this->getWidgetHooks(), $possible_hooks_list);
+            $possible_hooks_list = array_merge($this->getWidgetHooks(array_column($possible_hooks_list, 'id_hook')), $possible_hooks_list);
             $name_column = array_column($possible_hooks_list, 'name');
             array_multisort($name_column, SORT_ASC, $possible_hooks_list);
         }
